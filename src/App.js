56258eef2ad94e59286677724f5e72d09d106a60
Eugie Limpin <eugiellimpin@gmail.com>
2020-07-31 09:37:33 +0800

    init firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+
 import logo from './logo.svg';
 import './App.css';
 
+firebase.initializeApp({
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+});
+
+const db = firebase.firestore();
+
 function App() {
+  console.log(firebase);
+  console.log(db);
+
   return (
     <div className="App">
       <header className="App-header">
