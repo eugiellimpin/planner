5ba10e81c6d0c88a9fef85c6524717c9c28f4ad2
Eugie Limpin <eugiellimpin@gmail.com>
2020-07-31 09:37:33 +0800

    init firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
+
 import logo from './logo.svg';
 import './App.css';
 
+firebase.initializeApp({
+  apiKey: 'AIzaSyCYwZHf6_JKzQxw2RJmLUg48rAiCclaBgI',
+  authDomain: 'ryan-florence-planner-clone.firebaseapp.com',
+  projectId: 'ryan-florence-planner-clone',
+});
+
+const db = firebase.firestore();
+
 function App() {
+  console.log(firebase);
+  console.log(db);
+
   return (
     <div className="App">
       <header className="App-header">
