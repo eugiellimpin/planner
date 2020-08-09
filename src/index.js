b7a24b08d21abf0a0042d2ba7c66f4140c0f28d0
Eugie Limpin <eugiellimpin@gmail.com>
2020-08-09 20:10:09 +0800

    Implement login with Google

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import * as firebase from 'firebase/app';
-import 'firebase/firestore';
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
-import App from './App';
+import App from "./App";
 
-import './styles/tailwind.css';
+import "./styles/tailwind.css";
 
 firebase.initializeApp({
   apiKey: process.env.FIREBASE_API_KEY,
@@ -14,11 +15,52 @@ firebase.initializeApp({
 });
 
 const db = firebase.firestore();
-const todosRef = db.collection('todos')
+const todosRef = db.collection("todos");
+
+const authProvider = new firebase.auth.GoogleAuthProvider();
+
+const Login = () => {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((currentUser) => {
+      if (currentUser && !user) setUser(currentUser);
+    });
+  }, [user, setUser]);
+
+  const login = () =>
+    firebase
+      .auth()
+      .signInWithPopup(authProvider)
+      .then((result) => {
+        console.log(result.user);
+        setUser(result.user);
+      });
+
+  const logout = () =>
+    firebase
+      .auth()
+      .signOut()
+      .then(() => setUser(undefined));
+
+  return (
+    <div>
+      {!user && <button onClick={login}>login with Google</button>}
+
+      {user && (
+        <div>
+          Current logged in user: {user.displayName} ({user.email})
+          <button onClick={logout}>Logout</button>
+        </div>
+      )}
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
+    <Login />
     <App todosRef={todosRef} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
