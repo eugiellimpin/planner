f966ad2d5d2e6357f665773efeed29e10dc1f8cb
Eugie Limpin <eugiellimpin@gmail.com>
2020-08-10 21:08:54 +0800

    Refactor <App />

---
+++ src/Dashboard.js
@@ -0,0 +1,307 @@
+import React, { useState, useEffect } from "react";
+import * as firebase from "firebase/app";
+import getDaysInMonth from "date-fns/getDaysInMonth";
+import isDate from "date-fns/isDate";
+import c from "classnames";
+
+import { ReactComponent as PushLeftIcon } from "./assets/push_left.svg";
+import { ReactComponent as PushRightIcon } from "./assets/push_right.svg";
+import { ReactComponent as DeleteIcon } from "./assets/trash.svg";
+
+function IconButton({ children, className, ...props }) {
+  return (
+    <button
+      {...props}
+      className={c("hover:bg-gray-300 p-1 rounded", className)}
+    >
+      {children}
+    </button>
+  );
+}
+
+function createTimestamp(date) {
+  return new firebase.firestore.Timestamp(Math.floor(date.getTime() / 1000), 0);
+}
+
+function Todo({ todo, onMove, onUpdate, onDelete, moveButtonPosition }) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  return isEditing ? (
+    <TodoForm
+      todo={todo}
+      onSave={(title) => {
+        onUpdate({ id: todo.id, title });
+        setIsEditing(false);
+      }}
+      onCancel={() => setIsEditing(false)}
+    />
+  ) : (
+    <li className="flex items-center h-12 px-2 hover:bg-gray-100">
+      {moveButtonPosition === "left" && (
+        <IconButton onClick={onMove} className="mr-1">
+          <PushLeftIcon />
+        </IconButton>
+      )}
+
+      <input
+        type="checkbox"
+        checked={todo.done}
+        onChange={(e) =>
+          onUpdate({ id: todo.id, done: e.currentTarget.checked })
+        }
+      />
+      <span onClick={() => setIsEditing(true)} className="flex-grow px-2">
+        {todo.title}
+      </span>
+
+      <span>
+        <IconButton onClick={() => onDelete(todo.id)} className="mr-1">
+          <DeleteIcon />
+        </IconButton>
+
+        {moveButtonPosition === "right" && (
+          <IconButton onClick={onMove}>
+            <PushRightIcon />
+          </IconButton>
+        )}
+      </span>
+    </li>
+  );
+}
+
+function TodoForm({ onSave, todo, onCancel }) {
+  const [isEditing, setIsEditing] = useState(!!todo);
+  const [title, setTitle] = useState(!!todo ? todo.title : "");
+  const saveLabel = !!todo ? "Save" : "Add task";
+
+  return (
+    <li className="flex flex-col">
+      {isEditing && (
+        <textarea
+          value={title}
+          onChange={(e) => setTitle(e.currentTarget.value)}
+          className="rounded border border-gray-600 p-2"
+        />
+      )}
+
+      <div className="mt-2">
+        <button
+          disabled={isEditing && title.length < 1}
+          onClick={() => {
+            if (!isEditing) setIsEditing(true);
+
+            if (title.trim().length > 0) {
+              onSave(title);
+              setTitle("");
+            }
+          }}
+          className="px-2 py-1 rounded bg-green-600 text-white font-bold focus:outline-none"
+        >
+          {saveLabel}
+        </button>
+        {isEditing && (
+          <button
+            onClick={() => {
+              setIsEditing(false);
+              onCancel && onCancel();
+            }}
+            className="px-2 py-1 text-gray-700 hover:underline focus:outline-none"
+          >
+            Cancel
+          </button>
+        )}
+      </div>
+    </li>
+  );
+}
+
+function CalendarDay({ onClick, isCurrent, children }) {
+  return (
+    <div onClick={onClick} className={isCurrent ? "bg-lightRed" : ""}>
+      {children}
+    </div>
+  );
+}
+
+function Calendar({ onClickDay, isDisplayedDate, ...props }) {
+  const currentDate = new Date();
+  const dayCount = getDaysInMonth(currentDate);
+
+  return (
+    <div {...props}>
+      <h2>Calendar</h2>
+      <div className="grid-container">
+        {[...new Array(dayCount).keys()].map((dayIndex) => {
+          return (
+            <CalendarDay
+              key={dayIndex}
+              onClick={() => onClickDay(dayIndex + 1)}
+              isCurrent={isDisplayedDate(dayIndex + 1)}
+            >
+              {dayIndex + 1}
+            </CalendarDay>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+function TodoList({ todos, onMove, onUpdate, onDelete, moveButtonPosition }) {
+  return (
+    <ul>
+      {todos.map((t) => (
+        <Todo
+          todo={t}
+          onMove={() => onMove(t.id)}
+          onUpdate={onUpdate}
+          onDelete={onDelete}
+          moveButtonPosition={moveButtonPosition}
+          key={t.id}
+        />
+      ))}
+    </ul>
+  );
+}
+
+function Day({ date, todos, todosRef, onSave, onUpdate, onDelete, ...props }) {
+  const onMove = (id) => todosRef.doc(id).update({ dueDate: null });
+
+  return (
+    <div {...props}>
+      <h2>{date.toDateString()}</h2>
+      <TodoList
+        todos={todos}
+        onMove={onMove}
+        moveButtonPosition="right"
+        onUpdate={onUpdate}
+        onDelete={onDelete}
+      />
+
+      <TodoForm onSave={onSave} />
+    </div>
+  );
+}
+
+function Backlog({
+  onSave,
+  onUpdate,
+  todos,
+  todosRef,
+  displayedDate,
+  onDelete,
+  ...props
+}) {
+  const onMove = (id) =>
+    todosRef.doc(id).update({ dueDate: createTimestamp(displayedDate) });
+
+  return (
+    <div {...props}>
+      <h2>Backlog</h2>
+
+      <TodoList
+        todos={todos}
+        onMove={onMove}
+        onUpdate={onUpdate}
+        onDelete={onDelete}
+        moveButtonPosition="left"
+      />
+
+      <TodoForm onSave={(todo) => onSave(todo, null)} />
+    </div>
+  );
+}
+
+function Dashboard({ todosRef, user, onLogout }) {
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = today.getMonth();
+  const [todos, setTodos] = useState([]);
+  const [displayedDate, setDisplayedDate] = useState(today);
+
+  const onDelete = (id) => {
+    todosRef.doc(id).delete();
+  };
+
+  const onUpdate = ({ id, ...updatedTodo }) => {
+    todosRef.doc(id).update(updatedTodo);
+  };
+
+  const onSave = (dueDate) => (title) => {
+    todosRef.add({
+      title,
+      done: false,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      dueDate,
+    });
+  };
+
+  const changeDisplayedDate = (dayOfMonth) => {
+    setDisplayedDate(new Date(year, month, dayOfMonth));
+  };
+
+  const isDisplayedDate = (dayOfMonth) => {
+    if (isDate(dayOfMonth)) {
+      return dayOfMonth.toDateString() === displayedDate.toDateString();
+    }
+
+    return (
+      new Date(year, month, dayOfMonth).toDateString() ===
+      displayedDate.toDateString()
+    );
+  };
+
+  useEffect(() => {
+    const unsubscribe = todosRef.orderBy("createdAt").onSnapshot((snapshot) => {
+      const fetchedTodos = [];
+      snapshot.forEach((t) => {
+        fetchedTodos.push({ id: t.id, ...t.data() });
+      });
+
+      setTodos(fetchedTodos);
+    });
+
+    return unsubscribe;
+  }, [todosRef]);
+
+  const todosToday = todos.filter(
+    (t) => t.dueDate && isDisplayedDate(t.dueDate.toDate())
+  );
+  const backlogTodos = todos.filter((t) => !t.dueDate);
+
+  return (
+    <div>
+      <nav>
+        Current logged in user: {user.displayName} ({user.email})
+        <button onClick={onLogout}>Logout</button>
+      </nav>
+      <div className="flex">
+        <Calendar
+          onClickDay={changeDisplayedDate}
+          isDisplayedDate={isDisplayedDate}
+          className="column"
+        />
+        <Day
+          date={displayedDate}
+          todos={todosToday}
+          todosRef={todosRef}
+          onSave={onSave(createTimestamp(displayedDate))}
+          onUpdate={onUpdate}
+          onDelete={onDelete}
+          className="column"
+        />
+        <Backlog
+          onSave={onSave(null)}
+          onUpdate={onUpdate}
+          onDelete={onDelete}
+          todos={backlogTodos}
+          todosRef={todosRef}
+          displayedDate={displayedDate}
+          className="column"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
--- src/index.js
+++ src/index.js
@@ -1,66 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import ReactDOM from "react-dom";
-import * as firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
 
 import App from "./App";
 
 import "./styles/tailwind.css";
 
-firebase.initializeApp({
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-});
-
-const db = firebase.firestore();
-const todosRef = db.collection("todos");
-
-const authProvider = new firebase.auth.GoogleAuthProvider();
-
-const Login = () => {
-  const [user, setUser] = useState();
-
-  useEffect(() => {
-    firebase.auth().onAuthStateChanged((currentUser) => {
-      if (currentUser && !user) setUser(currentUser);
-    });
-  }, [user, setUser]);
-
-  const login = () =>
-    firebase
-      .auth()
-      .signInWithPopup(authProvider)
-      .then((result) => {
-        console.log(result.user);
-        setUser(result.user);
-      });
-
-  const logout = () =>
-    firebase
-      .auth()
-      .signOut()
-      .then(() => setUser(undefined));
-
-  return (
-    <div>
-      {!user && <button onClick={login}>login with Google</button>}
-
-      {user && (
-        <div>
-          Current logged in user: {user.displayName} ({user.email})
-          <button onClick={logout}>Logout</button>
-        </div>
-      )}
-    </div>
-  );
-};
-
 ReactDOM.render(
   <React.StrictMode>
-    <Login />
-    <App todosRef={todosRef} />
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
