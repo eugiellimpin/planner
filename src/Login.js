cd12e7a5f3261008a31c2175315296dd9b9b92ff
Eugie Limpin <eugiellimpin@gmail.com>
2020-08-11 18:49:12 +0800

    Style login page

--- src/Login.js
+++ src/Login.js
@@ -15,8 +15,16 @@ const Login = ({ onLoggedIn }) => {
       });
 
   return (
-    <div>
-      <button onClick={login}>login with Google</button>
+    <div className="bg-gray-100 flex h-screen">
+      <div className="flex flex-col justify-between bg-white rounded border h-40 w-64 mt-48 mx-auto p-5">
+        <h1 className="font-bold text-lg">Log in</h1>
+        <button
+          className="mb-4 py-2 text-sm border rounded block w-full focus:outline-none hover:bg-gray-200 hover:border-gray-300"
+          onClick={login}
+        >
+          Login with Google
+        </button>
+      </div>
     </div>
   );
 };
