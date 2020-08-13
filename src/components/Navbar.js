485b3d3e93bbf054bc004fb54b5180998905d1a1
Eugie Limpin <eugiellimpin@gmail.com>
2020-08-13 18:38:47 +0800

    Prettify

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,17 @@
 import React from "react";
 
 function Navbar({ user, onLogout }) {
-      return (<nav className="flex justify-between items-center h-12 shadow-md px-4">
-        Hello {user.displayName}! 👋
-        <button className="text-sm text-gray-600 hover:underline" onClick={onLogout}>Logout</button>
-      </nav>);
-
+  return (
+    <nav className="flex justify-between items-center h-12 shadow-md px-4">
+      Hello {user.displayName}! 👋
+      <button
+        className="text-sm text-gray-600 hover:underline"
+        onClick={onLogout}
+      >
+        Logout
+      </button>
+    </nav>
+  );
 }
 
 export default Navbar;
